90447e77bfd9527e673be1140defc0a42f8e5491
makun <michaelmakun@aliyun.com>
2017-02-18 14:37:01 +0800

    install fog

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.fog_privider = "fog"
+    config.fog_credentials = {
+      provider:         'AWS'
+      aws_access_key_id: ENV["AWE_ACCESS_KEY_ID"],
+
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+
+      region:    ENV["AWS_REGION"]
+    }
+    config.fog_directory = ENV["AWS_BUCKET_NAME"]
+
+  else
+    config.storage :file
+  end
+end
