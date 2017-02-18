81012b9fca7ca34b730bab5a2f603fe75485fba0
makun <michaelmakun@aliyun.com>
2017-02-18 14:55:38 +0800

    change carrierwave.rb AWE to AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     config.fog_provider = "fog"
     config.fog_credentials = {
       provider:         'AWS',
-      aws_access_key_id: ENV["AWE_ACCESS_KEY_ID"],
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
 
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
 
